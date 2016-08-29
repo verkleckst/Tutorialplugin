0e8386b5a114c7a4a8a93cdf484e52881f338451
verkleckst <uplayercrew@web.de>
2016-08-29 08:26:22 +0200

    Add files via upload

---
+++ src/de/verkleckst/Tutorial/Commands/CM_Heal.java
@@ -0,0 +1,29 @@
+package de.verkleckst.Tutorial.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.verkleckst.Tutorial.Hauptklasse;
+
+public class CM_Heal implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
+		
+		Player p = (Player)sender;
+		
+		if((sender instanceof Player))
+		{
+			p.setHealth(20D);
+			p.setHealthScale(20D);
+			p.setFoodLevel(20);
+			p.sendMessage(Hauptklasse.prefix + "§7Du wurdest §cgeheilt§7!");
+		}
+		
+		return true;
+	}
+
+}
---
+++ src/de/verkleckst/Tutorial/Hauptklasse.java
@@ -0,0 +1,38 @@
+package de.verkleckst.Tutorial;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.verkleckst.Tutorial.Commands.CM_Heal;
+
+public class Hauptklasse extends JavaPlugin{
+	
+	public static String prefix = "§8[§6TutorialPlugin§8] ";
+	
+	public void onEnable()
+	{
+		System.out.println("Tutorialplugin by verkleckst");
+		System.out.println("Plugin aktiviert");
+		
+		registerEvents();
+	}
+	
+	public void onDisable()
+	{
+		System.out.println("Tutorialplugin by verkleckst");
+		System.out.println("Plugin deaktiviert");
+	}
+	
+	public void registerEvents()
+	{
+		PluginManager pm = Bukkit.getPluginManager();
+		
+		//Events\\
+		
+		
+		//Commands\\
+		getCommand("heal").setExecutor(new CM_Heal());
+	}
+	
+}
